ca0b4c0badbcabf79080da79f4c61602f8a22575
eugenleshchov <eugenleshchov@gmail.com>
2017-01-15 02:44:14 +0300

    add styles to heroes list

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,14 +5,20 @@ export class Hero {
     name: string;
 }
 
+const HEROES: Hero[] = [
+    { id: 1, name: 'Alinanas' },
+    { id: 2, name: 'Eurenius' },
+    { id: 3, name: 'Kostya Fedirich'},
+    { id: 4, name: 'Mrs. Vospetka'},
+    { id: 5, name: 'Mr. Dekan'}
+];
+
 @Component({
     selector: "my-app",
-    templateUrl: "app/app.component.html"
+    templateUrl: "app/app.component.html",
+    styleUrls: ["app/heroes-list.css"]
 })
 export class AppComponent {
     title: string = 'Tour of Heroes';
-    hero: Hero = {
-        name: 'Windstorm',
-        id: 1
-    };
+    heroes = HEROES;
 }
