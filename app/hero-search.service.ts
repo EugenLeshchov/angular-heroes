75c54fd42287cc07d98b3fd9f9585685441112ce
eugenleshchov <eugenleshchov@gmail.com>
2017-01-18 12:35:16 +0300

    enhance hero search

--- app/hero-search.service.ts
+++ app/hero-search.service.ts
@@ -10,7 +10,7 @@ export class HeroSearchService {
         private http: Http
     ) {}
 
-    search(term: string) {
+    search(term: string): Observable<Hero[]> {
         return this.http
             .get(`app/heroes/?name=${term}`)
             .map((r: Response) => r.json().data as Hero[]);
